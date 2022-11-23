8b040a79c0fa63579dfb183a004df97ca1cd7fe5
Kieeary <70502804+Kieeary@users.noreply.github.com>
2022-11-23 21:16:13 +0900

    test

--- livart/src/main/java/connectionpool/MemberServlet.java
+++ livart/src/main/java/connectionpool/MemberServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+//test
 
 @WebServlet("/member2")
 public class MemberServlet extends HttpServlet {
