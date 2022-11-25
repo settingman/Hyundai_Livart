06c138b04b07518ab4ef95a51e8065537567fe87
Kieeary <70502804+Kieeary@users.noreply.github.com>
2022-11-25 21:28:01 +0900

    test

--- livart/src/main/java/controller/FrontController.java
+++ livart/src/main/java/controller/FrontController.java
@@ -38,7 +38,6 @@ public class FrontController extends HttpServlet{
         String requestURI = request.getRequestURI();
         ControllerLivart controller = controllerMap.get(requestURI);
         
-        
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
