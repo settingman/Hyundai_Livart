35126928e2d54e6faaf38bb3e5e673424e43bf19
settingman <116868123+settingman@users.noreply.github.com>
2022-11-25 17:46:25 +0900

    VO modify

--- livart/src/main/java/dto/MemberVO.java
+++ livart/src/main/java/dto/MemberVO.java
@@ -1,5 +1,17 @@
 package dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
 public class MemberVO {
+	
+	private String user_id;
+	private String pwd;
+	private String phone;
+	private String email;
+	private String name;
+	
+	
 
 }
